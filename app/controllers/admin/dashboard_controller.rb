da4b7d8bdc63b0bc57a4990728aa67a897dfb3c0
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2023-03-12 03:19:08 -0400

    Impelement counts for dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,10 @@
 class Admin::DashboardController < ApplicationController
+  
+  
   def show
+    @product_count = Product.all.count
+    #@category_count = Product.distinct.count('category_id')
+    @category_counts = Category.all.count
+  
   end
 end
