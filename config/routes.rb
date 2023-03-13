5660813cb2f4ed13f09966b3834fa792d54b3946
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2023-03-12 23:06:18 -0400

    Implement route for categories

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,14 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index]
   end
+
+
+  # namespace :admin do
+  #   get 'categories/index'
+  # end
+  # root to: 'products#index'
 
   # namespace :about do
   #   root to : "about#index"
