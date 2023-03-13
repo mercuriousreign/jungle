89e6df637b7ea8ca64c72aaea8f4f8c34512646a
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2023-03-13 01:50:41 -0400

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,13 +15,9 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-    resources :categories, only: [:index]
+    resources :categories, except: [:edit, :update, :show]
   end
-
-
-  # namespace :admin do
-  #   get 'categories/index'
-  # end
+  
   # root to: 'products#index'
 
   # namespace :about do
