542d3c0e06e6332fa0cf38f983974beda35c5e73
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2023-03-14 01:28:36 -0400

    implement session

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,10 @@ Rails.application.routes.draw do
   get 'sign_up', to: "users#new"
   post 'sign_up', to: "users#create"
   resources :confirmations, only: [:create, :edit, :new], param: :confirmation_token
+
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  get "login", to: "sessions#new"
   # get 'about/' => "about#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
