a2a82a560cfe6deab7a3a15aea859088900eebcf
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2023-03-15 00:55:26 -0400

    Implement cypress test for home page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,10 @@
+
+it("There is products on the page",()=> {
+  // cy.visit('http://localhost:3000/');
+  cy.visit('/')
+  cy.get(".products article").should("be.visible");
+});
+
+it("There is 2 products on the page", () => {
+  cy.get(".products article").should("have.length",2);
+});
