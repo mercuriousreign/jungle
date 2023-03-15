19e93b7c07feff85aa6bbcb1e869936473b9442c
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2023-03-14 23:44:44 -0400

    Add cypress to project

---
+++ config/initializers/cypress_rails.rb
@@ -0,0 +1,16 @@
+require 'database_cleaner/active_record'
+DatabaseCleaner.strategy = :truncation
+
+return unless Rails.env.test?
+
+CypressRails.hooks.before_server_start do
+end
+
+CypressRails.hooks.after_transaction_start do
+end
+
+CypressRails.hooks.after_state_reset do
+end
+
+CypressRails.hooks.before_server_stop do
+end
