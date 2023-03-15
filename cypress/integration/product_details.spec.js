b8b5aaad7f2cb31aa5672ca67aecc33d839a6a6f
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2023-03-15 01:06:16 -0400

    Implement homepage to product navigation test

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,10 @@
+it("There is products on the page",()=> {
+  cy.visit('http://localhost:3000/');
+  //cy.visit('/')
+  cy.get(".products article").should("be.visible");
+});
+
+it("There is 2 products on the page", () => {
+  cy.get(':nth-child(1) > a > img').click()
+  cy.get('.btn').contains("Add")
+});
