fe298c8479d3e2a35baf201175cd37c0f57b668d
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2023-03-15 17:42:23 -0400

    Implement http authentication to admin pages

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,7 +3,6 @@ class Admin::DashboardController < ApplicationController
   
   def show
     @product_count = Product.all.count
-    #@category_count = Product.distinct.count('category_id')
     @category_counts = Category.all.count
   
   end
